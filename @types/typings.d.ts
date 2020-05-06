cb65917e02ad67bbf3604329e9966607c94384b9
KiranMantha <kiran.manthavenkatas@rci.rogers.com>
2020-05-06 23:19:37 +0530

    added comments with reason for not using style-loader

--- @types/typings.d.ts
+++ @types/typings.d.ts
@@ -25,6 +25,10 @@ interface fetch {
 }
 
 declare module '*.scss' {
-  const content: any;
+	const content: any;
+	// using style-loader will result in an object which is incompatible
+	// hence use only css-loader and sass-loader which result in proper compiled css array 
+	// calling toString on compiled css array will result in proper css string 
+	// which will feed to component decorator
   export default content.toString();
 }
